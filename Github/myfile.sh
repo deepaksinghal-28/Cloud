1befdcafc2641aa74700ba721b7378a8bbc98501
Singhal <Deepak.Singhal@Mattel.com>
2026-04-30 01:07:50 +0530

    modified the myfile.sh in main branch

--- Github/myfile.sh
+++ Github/myfile.sh
@@ -8,6 +8,8 @@ echo "the sum of num1 $num1 and num2 $num2 is $num3"
 
 username="Deepak"
 
-echo "This file is created by $username"
+echo "this file is modified by username"
+
+echo "it is modified in main branch by username"
 
 
