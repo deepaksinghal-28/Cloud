e1a0493b58d028254d1518f4973c511d3a332b31
Singhal <Deepak.Singhal@Mattel.com>
2026-04-29 22:26:25 +0530

    modified the myfile.sh

--- Github/myfile.sh
+++ Github/myfile.sh
@@ -4,7 +4,10 @@ num1=100
 num2=200
 num3=$((num1+num2))
 
+echo "the sum of num1 $num1 and num2 $num2 is $num3"
+
 username="Deepak"
-echo "This file is created by username"
+
+echo "This file is created by $username"
 
 
